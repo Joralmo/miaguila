48ca39cb58953e14f8fc15ab3a999e91d07f1792
JoralmoPro <JoralmoPro@gmail.com>
2020-08-13 15:41:30 -0500

    instanciando clase database y conentando a la base de datos 😎

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import Server from './helpers/server';
 import { Request, Response } from 'express';
+import Database from './database';
 
 const server = new Server();
 
@@ -10,5 +11,15 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 server.start(async () => {
-    console.log('Running');
+    try {
+        const database = Database.getInstance();
+        await database.initDb();
+        console.log(
+            'Conectado a la base de datos',
+            database.conn.connection.name,
+        );
+        console.log('Running');
+    } catch (error) {
+        console.log('Error al conectarse =>', error);
+    }
 });
