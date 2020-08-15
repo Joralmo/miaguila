bc705a69085e02c3b02a023228f25b1e93781207
JoralmoPro <JoralmoPro@gmail.com>
2020-08-14 21:02:03 -0500

    utilizando variables de entorno para conectar a la base de datos 😎

--- database/index.ts
+++ database/index.ts
@@ -1,18 +1,25 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 
 export default class Database {
     private static instance: Database;
     public conn: any = null;
     private options: mongoose.ConnectionOptions;
     private constructor() {
+        if (process.env.NODE_ENV === 'dev') {
+            dotenv.config({ path: path.join(__dirname, '../env/env.dev') });
+        } else {
+            dotenv.config({ path: path.join(__dirname, '../env/env.prod') });
+        }
         this.options = {
             bufferCommands: false,
             bufferMaxEntries: 0,
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            user: 'root',
-            pass: 'example',
-            dbName: 'miaguila',
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD,
+            dbName: process.env.DB_NAME,
         };
         this.conn = null;
     }
@@ -32,7 +39,7 @@ export default class Database {
         if (this.conn === null) {
             try {
                 this.conn = await mongoose
-                    .connect('mongodb://localhost:27017/admin', this.options)
+                    .connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/admin`, this.options)
                     .catch(err => {
                         throw new Error(err);
                     });
